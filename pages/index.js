e139728f49508566cc1ab190ef06dc99b7b39d07
Creele <52602586+creele@users.noreply.github.com>
2026-03-25 20:48:42 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import PastorChaplainUSAWebsite from '../pastor_chaplain_usa_website';
+import PastorChaplainUSAWebsite from "../pastor_chaplain_usa_website";
 
 export default function Home() {
   return <PastorChaplainUSAWebsite />;
